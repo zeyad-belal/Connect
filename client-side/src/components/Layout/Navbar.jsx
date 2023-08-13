c1ca4a1d347b930d208df7e27bc11b361ba5acb7
zeyad belal <zeyadbelal00@gmail.com>
2023-08-13 18:05:39 +0300

    menusNigthmareDone

--- client-side/src/components/Layout/Navbar.jsx
+++ client-side/src/components/Layout/Navbar.jsx
@@ -13,6 +13,8 @@ import { FiMenu } from "react-icons/fi";
 import SubNav from "./SubNav";
 import { MdAdd } from "react-icons/md";
 import { BiSolidTruck } from "react-icons/bi";
+import { RxAvatar } from "react-icons/rx";
+import { FaSignOutAlt } from "react-icons/fa";
 import { Backdrop } from "../../UI/Modal";
 
 const Navbar = (props) => {
@@ -23,23 +25,44 @@ const Navbar = (props) => {
   const [cookies, setCookie, removeCookie] = useCookies(["UserToken", "User"]);
   const [CurrUser, setCurrUser] = useState("");
   const [subIsVisible, setSubIsVisible] = useState(false);
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
+  const [isNotiMenuVisible, setIsNotiMenuVisible] = useState(false);
+  const [searchBarIsVisible, setSearchBarIsVisible] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsDropdownVisible(true);
-  };
 
-  const handleMouseLeave = () => {
-    setIsDropdownVisible(false);
+  const toggleUserMenu = () => {
+    setIsNotiMenuVisible(false)
+    setSearchBarIsVisible(false)
+    setSubIsVisible(false);
+    setIsUserMenuVisible(prevValue => !prevValue);
   };
+  const toggleNotiMenu = () => {
+    setIsUserMenuVisible(false)
+    setSearchBarIsVisible(false)
+    setSubIsVisible(false);
+    setIsNotiMenuVisible(prevValue => !prevValue);
+  };
+  
+  function toggleSubNav(){
+    setIsUserMenuVisible(false);
+    setIsNotiMenuVisible(false);
+    setSubIsVisible(prevValue => !prevValue)
+  }
+  
+  function toggleSearchBar(){
+    setIsUserMenuVisible(false);
+    setIsNotiMenuVisible(false);
+    setSubIsVisible(false);
+    setSearchBarIsVisible(prevState => !prevState)
+  }
 
-  const handleDropdownMouseEnter = () => {
-    setIsDropdownVisible(true);
-  };
-
-  const handleDropdownMouseLeave = () => {
-    setIsDropdownVisible(false);
-  };
+  
+  function closeAllMenus(){
+    setIsNotiMenuVisible(false);
+    setIsUserMenuVisible(false);
+    setSearchBarIsVisible(false);
+    setSubIsVisible(false);
+  }
 
 
   useEffect(() => {
@@ -58,30 +81,19 @@ const Navbar = (props) => {
     window.location.reload();
   }
 
-function closeTheSearchBar(e){
-  if(!e.target.classList.contains("search")){
-    props.closeSearchBar()
-  }
-}
-
-function toggleSubNav(){
-  setSubIsVisible(prevValue => !prevValue)
-}
 
   return (
     <>
       <nav
         id="MainNav"
-        className=" relative  top-0  bg-primary text-white  my-30 h-15 w-full z-50 sm:px-7 px-4 flex justify-between "
-        onClick={(e)=>closeTheSearchBar(e)}
-      >
+        className=" relative  top-0  bg-primary text-white  my-30 h-15 w-full z-50 sm:px-7 px-4 flex justify-between " >
         <ul className="flex ">
         <li className=" mr-3 cursor-pointer text-lg flex items-center py-2 px-6 text-white hover:bg-gray-500" onClick={toggleSubNav}>
         <FiMenu size={30} />
         </li>
         {/* Logo */}
         <Link to={"/"}>
-          <li className="flex items-baseline py-2 mr-4 w-44 sm:w-56">
+          <li className="flex items-baseline py-2 mr-4 w-44 sm:w-56" onClick={closeAllMenus}>
             <img
               className="max-w-[220px]"
               src="/assets/logo/main-yellow-and-white.png"
@@ -94,26 +106,44 @@ function toggleSubNav(){
         <li className="text-md flex items-center py-2 px-4 text-white hover:bg-gray-500  cursor-pointer gap-2"><BiSolidTruck size={22} /> Orders</li>
         </ul>
         
-
-        {/* Navigation */}
+      {/* ----------------------------------------------------------------------------- */}
         <ul className="flex ">
           {/* Searchbar  */}
-          <li className={props.searchBarIsVisible ?
+          <li className={searchBarIsVisible ?
             "search flex items-center py-2 px-6 text-white bg-gray-500  hover:bg-gray-500 cursor-pointer" :
-            "search flex items-center py-2 px-6 text-white  hover:bg-gray-500 cursor-pointer"} onClick={props.toggleSearchBar}>
+            "search flex items-center py-2 px-6 text-white  hover:bg-gray-500 cursor-pointer"} onClick={toggleSearchBar}>
             <BsSearch size={20} />
           </li>
 
           {/* notifcations  */}
-          <li className="flex items-center py-2 px-6 text-white hover:bg-gray-500  cursor-pointer">
+          <li className="flex items-center py-2 px-6 text-white hover:bg-gray-500  cursor-pointer" onClick={toggleNotiMenu}>
             <BsFillBellFill size={20} />
+
+            {isNotiMenuVisible && (
+              <>
+              <div className="relative z-30">
+                      <ul className="flex flex-col gap-3 absolute right-[-21px]   bottom-[-117px] min-w-[150px] bg-white  shadow-md border border-gray-300">
+                        <div className="absolute before:w-0  before:h-0  before:transform before:-rotate-45  before:border-white before:border-8 before:bg-white before:absolute before:top-[-3px] before:right-[-127px]"></div>
+                          
+                          <li className="flex gap-2 items-center z-10 py-2 pl-2 pr-6  text-sm font-semibold text-primary hover:bg-gray-300 cursor-pointer" 
+                          > <RxAvatar size={22} /> {CurrUser.first_name} {CurrUser.last_name}</li>
+                          <li
+                            onClick={signoutHandler}
+                            className=" flex gap-2 items-center py-2 pl-2 pr-6  text-sm font-semibold text-primary hover:bg-gray-300 cursor-pointer" >
+                            <FaSignOutAlt className="ml-1" size={20} /> Sign Out{" "}
+                          </li>
+                        </ul>
+              </div>
+              {/* <div className=" fixed top-0 left-0 w-full h-screen z-20 bg-opacity-40"></div> */}
+              </>
+                )}
           </li>
           {/* cart  */}
             <Link
               className="text-white flex items-center relative hover:bg-gray-500 "
               to="/cart" >
                 <li className="flex items-center py-2 px-6  ">
-                <FaShoppingCart size={20} />
+                <FaShoppingCart size={20} onClick={closeAllMenus} />
                 {cartCTX.totalItemsNum > 0 && (
                   <span className="ml-1 bg-f37020 text-white rounded-full px-[7px] py-[1px] text-[14px] absolute right-[-20px] top-[-17px]">
                     {cartCTX.totalItemsNum}
@@ -122,44 +152,46 @@ function toggleSubNav(){
                 </li>
             </Link>
           {/* user  */}
-          <li className="flex items-center py-2 px-6 text-white hover:bg-gray-500  text-sm:10 ">
+          <li className="flex items-center py-2 px-6 text-white  text-sm:10 " >
             {userStatus ? (
-              <div className="relative  border-black">
+              <div className="relative  border-black" onClick={toggleUserMenu}>
                 <img
                   className="max-w-[40px] rounded-full cursor-pointer border-inner border-black"
                   src={CurrUser.avatar}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
                   alt="User Avatar"
                 />
 
-                {isDropdownVisible && (
-                  <div
-                    onMouseEnter={handleDropdownMouseEnter}
-                    onMouseLeave={handleDropdownMouseLeave}
-                    className="absolute right-9 top-4  py-1 w-[100px] bg-white rounded-tl-full rounded-bl-full rounded-br-full shadow-lg"
-                  >
-                    <button
-                      onClick={signoutHandler}
-                      className="block w-full text-sm px-4 py-1 font-semibold text-gray-800 rounded-full hover:text-f37020"
-                    >
-                      Sign Out{" "}
-                    </button>
-                  </div>
+                {isUserMenuVisible && (
+                  <>
+                    <div className="relative z-30">
+                      <ul className="flex flex-col gap-3  absolute right-[-10px]   bottom-[-97px] min-w-[150px] bg-white  shadow-md border border-gray-300">
+                        <div className="absolute before:w-0  before:h-0  before:transform before:-rotate-45  before:border-white before:border-8 before:bg-white before:absolute before:top-[-3px] before:right-[-127px]"></div>
+                          
+                          <li className="flex gap-2 items-center z-10 py-2 pl-2 pr-6  text-sm font-semibold text-primary hover:bg-gray-300 cursor-pointer" 
+                          > <RxAvatar size={22} /> {CurrUser.first_name} {CurrUser.last_name}</li>
+                          <li
+                            onClick={signoutHandler}
+                            className=" flex gap-2 items-center py-2 pl-2 pr-6  text-sm font-semibold text-primary hover:bg-gray-300 cursor-pointer" >
+                            <FaSignOutAlt className="ml-1" size={20} /> Sign Out{" "}
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <div className=" fixed top-0 left-0 w-full h-screen z-20 bg-opacity-40"></div> */}
+                  </>
                 )}
               </div>
             ) : (
               <button
                 className="hover:text-gray-400 whitespace-nowrap text-[15px] ml-1"
-                onClick={() => userCTX.toggleModal()}
-              >
+                onClick={() => userCTX.toggleModal()} >
                 Sign in
               </button>
             )}
           </li>
         </ul>
       </nav>
-      <Searchbar {...props} searchBarIsVisible={props.searchBarIsVisible} />
+      <Searchbar {...props} searchBarIsVisible={searchBarIsVisible} />
+      {isNotiMenuVisible || isUserMenuVisible || searchBarIsVisible ? <div className=" fixed top-0 left-0 w-full h-screen z-20 bg-opacity-40" onClick={closeAllMenus}></div>: ''}
 {/* -------------------------------------------------------------------------------------------------------------- */}
         {subIsVisible? <Backdrop  toggleModal={toggleSubNav} /> : ''}
         <SubNav {...props} subIsVisible={subIsVisible} toggleSubNav={toggleSubNav} />
