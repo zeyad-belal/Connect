d2ca2916b53097af6e1a57a62baed910f2b493d7
zeyad belal <zeyadbelal00@gmail.com>
2023-08-16 19:19:06 +0300

    userSliceFix

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -6,21 +6,18 @@ import { ToastContainer } from "react-toastify";
 import {Provider} from  "react-redux";
 import store from "./store/store";
 
-import UserProvider from "./store/UserProvider";
 import { ServiceProvider } from "./store/ServicesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
 
     <ServiceProvider>
-      <UserProvider>
 
       <Provider store={store}>
           <ToastContainer />
           <App />
       </Provider>
 
-      </UserProvider>
     </ServiceProvider>
 
   </BrowserRouter>
