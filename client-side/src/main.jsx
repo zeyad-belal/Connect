dd9add1a7080b85050e7afb5655677e88e144382
zeyad belal <zeyadbelal00@gmail.com>
2023-10-07 14:21:32 +0300

    styleFix

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -4,11 +4,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import {Provider} from  "react-redux";
 import store from "./store/store";
-
+import ScrollToTop from "./ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
       <Provider store={store}>
+      <ScrollToTop />
           <App />
       </Provider>
   </BrowserRouter>
